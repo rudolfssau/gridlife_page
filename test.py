0febe80e3e11717e10fe2150acfb60b286cc71b8
rudolfssau <rudolfssau@gmail.com>
2020-10-14 19:17:05 +0300

    Adds Beautifulsoup updates

--- test.py
+++ test.py
@@ -3,47 +3,58 @@ from bs4 import BeautifulSoup
 import json
 import requests
 
-
 r = requests.get("https://www.trackdays.co.uk/track-days/brands-hatch/")
 soup = BeautifulSoup(r.text, 'html.parser')
 
-# writer.writerow(["Date, Track Fromat, Track Name, Price"])
-
 # Finds all of the days and the dd/mm/yy (example: Tue 27/10/20)
 
+numberOfDay = []
 days = soup.find_all(class_="record-column mob-col")
 for day in days:
     exactday = day.find_all("div", {"class": "record-txt"})
     for day in exactday:
-        print(day.text)
+        numberOfDay.append (day.text)
+        print(numberOfDay)
 
 # Finds all of the Venues (example: Brands Hatch)
 
+nameOfVenues = []
 for venueName in soup.find_all("div", {"class": "record-column"}):
     if "Hatch" in venueName.text:
-        venueName.text.strip()
+        nameOfVenues.append (venueName.text.strip())
+        print(nameOfVenues)
 
 # Finds all of the names of the track (example: Indy Circuit)
 
+trackNames = []
 for trackName in soup.find_all(class_="record-column column-center"):
     if "circuit" in trackName.text:
-        trackName.text.strip()
+        trackNames.append (trackName.text.strip())
+        print(trackNames)
 
 # Finds all of the specific track formats (example: 4 Groups p/h)
 
+formats = []
 for format in soup.find_all("div", {"class": "record-column pad-hide column-center"}):
     if "Pitlane" in format.text:
-        format.text.strip()
+        formats.append (format.text.strip())
+        print(formats)
 
 for format in soup.find_all("div", {"class": "record-column pad-hide column-center"}):
     if "p/h" in format.text:
-        format.text.strip()
+        formats.append (format.text.strip())
+        print(formats)
 
 for format in soup.find_all("div", {"class": "record-column pad-hide column-center"}):
     if "Track Day" in format.text:
-        format.text.strip()
+        formats.append (format.text.strip())
+        print(formats)
+
 
 finishedFormat = ["Date", "Track Name", "Track Venue Name", "Track Fromat"]
+trackInfo = (numberOfDay + nameOfVenues + trackNames + formats)
 
-with open("tracklistinfo.json", "w") as writeJSON:
-    json.dump(finishedFormat, writeJSON, ensure_ascii=False)
+
+with open("tracklistinfo.json", "w") as write_file:
+    json.dump(trackInfo, write_file, indent=4)
+
