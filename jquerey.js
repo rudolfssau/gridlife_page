61eabaf788f48c830c4f1f658e129c17336f4d13
rudolfssau <rudolfssau@gmail.com>
2020-10-31 23:29:22 +0200

    Adds jQuery updates

--- jquerey.js
+++ jquerey.js
@@ -4,15 +4,24 @@ function getList() {
         url: "JSON_Data/trackInfo.json"
     })
 }
+$.getJSON("JSON_Data/trackInfo.json", function(response) {
+    var janis = response[1];
+    var janisText = janis[1].DateShortened;
+    // console.log(ajn)
+    janis.forEach(function(elCreate) {
+        var elCreateDiv = document.createElement("div");
+        elCreateDiv.appendChild(document.createTextNode(response[1][1].DateShortened));
+        document.body.appendChild(elCreateDiv)
+    })
+});
 getList().done(function (response) {
     var data = JSON.parse(response);
     if (data != null) {
         jQuery.each(data, function (index, value) {
-            console.log(value[0].Date)
             $("#tDate").append('<td>' + "Date: " + value[0].Date + '</td>')
-            $("#tVenue").append('<td>' + "Venue: " + value[1].Venue + '</td>')
-            $("#tTrack").append('<td>' + "Track Name: " + value[2].Track + '</td>')
-            $("#tFormat").append('<td>' + "Track Format: " + value[3].Format + '</td>')
+            $("#tVenue").append('<td>' + "Venue: " + value[2].Venue + '</td>')
+            $("#tTrack").append('<td>' + "Track Name: " + value[3].Track + '</td>')
+            $("#tFormat").append('<td>' + "Track Format: " + value[4].Format + '</td>')
         })
     }
 })
