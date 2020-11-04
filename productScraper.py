3d83e9dfc73b8fd51e3b38cf0475ae3d5cd54148
rudolfssau <rudolfssau@gmail.com>
2020-11-04 20:50:14 +0200

    Adds a quick markup to Latest News section

--- productScraper.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import json
-import requests
-
-r = requests.get("https://shop.grid.life/")
-soup = BeautifulSoup(r.text, 'html.parser')
-
-janis = soup.find(class_="grid grid--uniform grid--view-items")
-print(janis)
