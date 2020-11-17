247480e7b33f3933984fe8b14d91fac16f98dfe3
rudolfssau <rudolfssau@gmail.com>
2020-11-17 16:57:54 +0200

    Adds fixes with styling for Contact Us form

--- jquerey.js
+++ jquerey.js
@@ -45,20 +45,26 @@ $(document).ready(function () {
         var lastn = $("#lastn").val()
         var email = $("#email").val()
         var subject = $("#subject").val()
-        var statusElm = $("#status")
-        statusElm.empty()
-        if(email.length > 5 && email.includes("@") && email.italics(".")) {
+        var invalidfn = $("#invalidfirstn")
+        var invalidln = $("#invalidlastn")
+        var invalidemail = $("#invalidemail")
+        invalidfn.empty()
+        invalidemail.empty()
+        invalidln.empty()
+        if(email.length > 5 && email.includes("@") && email.includes(".")) {
         } else {
-            statusElm.append("<div>That Email isn't valid</div>")
-        }
-        if(firstn.length < 2 && firstn.includes(".")) {
-            statusElm.append("<div>Invalid Name</div>")
+            event.preventDefault()
+            invalidemail.append("<div>Invalid Email</div>")
         }
         if(firstn.length < 2 || firstn.includes("1") || firstn.includes("2") || firstn.includes("3") || firstn.includes("4") || firstn.includes("4") || firstn.includes("5") || firstn.includes("6") || firstn.includes("7") || firstn.includes(".") || firstn.includes(";") || firstn.includes("!") || firstn.includes("?")) {
-            statusElm.append("<div>Invalid First Name</div>")
+            event.preventDefault()
+            document.querySelector("#invalidfirstn").style.display = "flex";
+            document.querySelector("#invalidfirstn").style.justifyContent = "center";
+            invalidfn.append("<div>Invalid First Name</div>")
         }
         if(lastn.length < 2 || lastn.includes("1") || lastn.includes("2") || lastn.includes("3") || lastn.includes("4") || lastn.includes("4") || lastn.includes("5") || lastn.includes("6") || lastn.includes("7") || lastn.includes(".") || lastn.includes(";") || lastn.includes("!") || lastn.includes("?")) {
-            statusElm.append("<div>Invalid Last Name</div>")
+            event.preventDefault()
+            invalidln.append("<div>Invalid Last Name</div>")
         }
     });
 });
