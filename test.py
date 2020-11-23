899bbde7812a932accd2041eacd95f616bd3620f
rudolfssau <rudolfssau@gmail.com>
2020-11-23 09:09:38 +0200

    Adds updates to webscraper

--- test.py
+++ test.py
@@ -5,12 +5,14 @@ import requests
 r = requests.get("https://www.trackdays.co.uk/track-days/brands-hatch/")
 soup = BeautifulSoup(r.text, 'html.parser')
 
-days = soup.find_all(class_="ddd-hide")
+days_name = soup.find_all(class_="ddd-hide")
+days_numbered = soup.find_all(class_="ddd-hide")
 
-print(days)
+
 # for row in rows:
 
-
+janis = [days_name.text.strip() for days_name in days_name]
+print(janis)
 # <div class="record-txt"><span class="ddd-hide">Mon</span> 07/12/20</div>
-# with open("Trackname_table/test.json", "w") as write_file:
-#                 json.dump(janis, write_file, indent=4)
+with open("Trackname_table/test.json", "w") as write_file:
+        json.dump(janis, write_file, indent=4)
