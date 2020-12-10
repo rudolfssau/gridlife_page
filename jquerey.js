e1cbbcb96630040a3a07494c39d66c89c28816ab
rudolfssau <rudolfssau@gmail.com>
2020-12-10 23:48:30 +0200

    Adds refreshed validation rules to Contact Us

--- jquerey.js
+++ jquerey.js
@@ -67,67 +67,10 @@ $(document).ready(function () {
         invalidsubj.empty();
         invalidcountry.empty();
         empty.empty();
-        // emptyFields.empty();
+
+
         var pjoutfStyle = document.querySelector("#pfotrFields").style.display;
         var emptyStyle = document.querySelector("#empty").style.display;
-
-        // if (email.length > 0 && firstn.length > 0 && lastn.length > 0 && subject.length > 0) {
-        //     if (country === "select") {
-        //         event.preventDefault();
-        //         document.querySelector("#invalidcountry").style.display = "flex";
-        //         document.querySelector("#invalidcountry").style.justifyContent = "center";
-        //         invalidcountry.append("<div>Please Select Your Country</div>")
-        //     } else {
-        //         document.querySelector("#invalidcountry").style.display = "none";
-        //     }
-        //     if (email.length > 5 && email.includes("@") && email.includes(".")) {
-        //         document.querySelector("#invalidemail").style.display = "none";
-        //     } else {
-        //         event.preventDefault()
-        //         document.querySelector("#invalidemail").style.display = "flex";
-        //         document.querySelector("#invalidemail").style.justifyContent = "center";
-        //         invalidemail.append("<div>Invalid Email</div>")
-        //     }
-        //     if (firstn.length < 2 || firstn.includes("1") || firstn.includes("2") || firstn.includes("3") || firstn.includes("4") || firstn.includes("4") || firstn.includes("5") || firstn.includes("6") || firstn.includes("7") || firstn.includes(".") || firstn.includes(";") || firstn.includes("!") || firstn.includes("?")) {
-        //         event.preventDefault()
-        //         document.querySelector("#invalidfirstn").style.display = "flex";
-        //         document.querySelector("#invalidfirstn").style.justifyContent = "center";
-        //         invalidfn.append("<div>Invalid First Name</div>")
-        //     } else {
-        //         document.querySelector("#invalidfirstn").style.display = "none";
-        //     }
-        //     if (lastn.length < 2 || lastn.includes("1") || lastn.includes("2") || lastn.includes("3") || lastn.includes("4") || lastn.includes("4") || lastn.includes("5") || lastn.includes("6") || lastn.includes("7") || lastn.includes(".") || lastn.includes(";") || lastn.includes("!") || lastn.includes("?")) {
-        //         event.preventDefault()
-        //         document.querySelector("#invalidlastn").style.display = "flex";
-        //         document.querySelector("#invalidlastn").style.justifyContent = "center";
-        //         invalidln.append("<div>Invalid Last Name</div>")
-        //     } else {
-        //         document.querySelector("#invalidlastn").style.display = "none";
-        //     }
-        //     if (subject.length == 0) {
-        //         event.preventDefault()
-        //         document.querySelector("#invalidsubject").style.display = "flex";
-        //         document.querySelector("#invalidsubject").style.justifyContent = "center";
-        //         invalidsubj.append("<div>Subject is empty</div>")
-        //     } else {
-        //         document.querySelector("#invalidsubject").style.display = "none";
-        //     }
-        // }
-        //
-
-
-
-
-
-
-
-
-
-        //Lai uztaisitu to Please FIll out All of the requiered fields, katram validation rule ir vajadzigs uztaisit velvienu ieksejo if stataemnt kas parbauda vai jau ir sie parametri ielikti
-        //     document.querySelector("#pfotrFields").style.display = "flex";
-        //     document.querySelector("#pfotrFields").style.justifyContent = "center";
-        //     emptyFields.append("<div>Please Fill Out The Remainig Fields</div>")
-        // Ja nav, tad izejot no if statemnt to enablo
 
 
         if (country == "select" || email.length == 0 || firstn.length == 0 || lastn.length == 0 || subject.length == 0) {
@@ -224,25 +167,26 @@ $(document).ready(function () {
 
         //Country Select validation rules
 
-        // if (country == "select") {
-        //     event.preventDefault()
-        //     document.querySelector("#empty").style.display = "none";
-        //     if (country == "select") {
-        //         event.preventDefault();
-        //         document.querySelector("#invalidcountry").style.display = "flex";
-        //         document.querySelector("#invalidcountry").style.justifyContent = "center";
-        //         invalidcountry.append("<div>Please Select Your Country</div>")
-        //     }
-        // }
-        // if (country != "select" || email.length == 0 || firstn.length == 0 || lastn.length == 0 || subject.length == 0) {
-        //     if (pjoutfStyle == "flex") {
-        //         event.preventDefault()
-        //     } else if (pjoutfStyle != "flex") {
-        //         event.preventDefault()
-        //         document.querySelector("#pfotrFields").style.display = "flex";
-        //         document.querySelector("#pfotrFields").style.justifyContent = "center";
-        //     }
-        // }
+        if (country == "select") {
+            event.preventDefault()
+            document.querySelector("#empty").style.display = "none";
+            if (country == "select") {
+                event.preventDefault();
+                document.querySelector("#invalidcountry").style.display = "flex";
+                document.querySelector("#invalidcountry").style.justifyContent = "center";
+                invalidcountry.append("<div>Please Select Your Country</div>")
+            }
+            if (country != "select" || email.length == 0 || firstn.length == 0 || lastn.length == 0 || subject.length == 0) {
+                if (pjoutfStyle == "flex") {
+                    event.preventDefault()
+                } else if (pjoutfStyle != "flex") {
+                    event.preventDefault()
+                    document.querySelector("#invalidcountry").style.display = "none";
+                    document.querySelector("#pfotrFields").style.display = "flex";
+                    document.querySelector("#pfotrFields").style.justifyContent = "center";
+                }
+            }
+        }
 
 
         //Subject validation rules
