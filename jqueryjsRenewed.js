49de7b5c02b897a1b7f42908f06033c687c89558
rudolfssau <rudolfssau@gmail.com>
2020-12-15 00:17:29 +0200

    Adds new validation rules to Contact Us

--- jqueryjsRenewed.js
+++ jqueryjsRenewed.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#submit").click(function (event) {
-        event.preventDefault()
+        // event.preventDefault()
         var firstn = $("#firstn").val();
         var lastn = $("#lastn").val();
         var email = $("#email").val();
@@ -19,7 +19,37 @@ $(document).ready(function () {
         invalidsubj.empty();
         invalidcountry.empty();
         empty.empty();
-        console.log("workds")
+        if (firstn.length < 2 || firstn.includes("!") || firstn.includes("@")) {
+            event.preventDefault()
+            document.querySelector("#invalidfirstn").style.display = "flex";
+            document.querySelector("#invalidfirstn").style.justifyContent = "center";
+            invalidfn.append("<div>Invalid First Name</div>")
+        }
+        if (firstn.length > 2) {
+            document.querySelector("#invalidfirstn").style.display = "none";
+        }
+
+
+        if (lastn.length < 2) {
+            event.preventDefault()
+            document.querySelector("#invalidlastn").style.display = "flex";
+            document.querySelector("#invalidlastn").style.justifyContent = "center";
+            invalidln.append("<div>Invalid Last Name</div>")
+        }
+        if (lastn.length > 2) {
+            document.querySelector("#invalidlastn").style.display = "none";
+        }
+
+
+        if (email.length < 5) {
+            event.preventDefault()
+            document.querySelector("#invalidemail").style.display = "flex";
+            document.querySelector("#invalidemail").style.justifyContent = "center";
+            invalidemail.append("<div>Invalid Email</div>")
+        }
+        if (email.length > 5 && email.includes("@") && email.includes(".")) {
+            document.querySelector("#invalidemail").style.display = "none";
+        }
     });
 });
 
