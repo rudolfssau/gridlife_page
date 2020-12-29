1657d93da19898f6821053833d8923fcf5c10e79
rudolfssau <rudolfssau@gmail.com>
2020-12-29 11:53:57 +0200

    Fixes bugs with firebase connection

--- firebase_signupforum.js
+++ firebase_signupforum.js
@@ -7,11 +7,12 @@ var firebaseConfig = {
     messagingSenderId: "863936980341",
     appId: "1:863936980341:web:d86f9ec52ef063e25a9f3c"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Refrence collection for firebase
-var collectionRef = firebase.database().ref("subject");
+var collectionRef = firebase.database().ref("newSignUp");
 
 //Submit forum
 document.getElementById("signUpForum").addEventListener('submit', submitformum);
@@ -39,6 +40,7 @@ function submitformum(a) {
 
     //Hide alert after completing function
     setTimeout(function () {
+        document.querySelector("#signUpForum-content").style.display = "none";
         document.querySelector("#sUFsuccessful").style.display = "none";
         document.querySelector("#signUpForum").style.display = "none";
     },3000);
@@ -49,8 +51,8 @@ function getInputValues(id) {
 
 //Save to Firebase
 function saveMessage(firstN, lastN, age, email, state, carManufacturer, carModel, carMY, carMods, carDrivetrain, carClass, additionalInfo) {
-    var newSubjectRef = collectionRef.push();
-    newSubjectRef.set({
+    var newcollectionRef = collectionRef.push();
+    newcollectionRef.set({
         firstN: firstN,
         lastN: lastN,
         age: age,
